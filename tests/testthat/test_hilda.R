4cbfc579d26a487c42a91d96d34510e2f65a5586
laeRusers <zhiyang@usc.edu>
2019-06-05 23:11:13 -0700

    remove git track for .DS_Store

--- tests/testthat/test_hilda.R
+++ tests/testthat/test_hilda.R
@@ -3,16 +3,15 @@ test_that("correctly load the test data", {
   load(inputFile)
 
   expect_equal(as.character(class(G)), "MutationFeatureData")
-  expect_equal(G@type, "independent")
-  expect_equal(G@flankingBasesNum, 5)
+  expect_equal(slot(G, "type"), "independent")
+  expect_equal(slot(G, "flankingBasesNum"), 5)
 })
 
 
 test_that("running the global test and the local test", {
-  inputFile <- system.file("extdata/sample.rdata", package = "HiLDA")
-  load(inputFile)
-  K <- 3
-  Param <- pmsignature::getPMSignature(G, K = K)
+
+  load(inputFile <- system.file("extdata/sample.rdata", package = "HiLDA"))
+  load(inputFile <- system.file("extdata/sampleParam.rdata", package = "HiLDA"))
 
   expect_equal(class(pmBarplot(G, Param, refGroup=1:4)), "list")
 })
